0f31e908c679e1f5ed3652a111af9e11ad49ef34
Sebastian Neuner <sebastiann@selfnet.de>
2013-02-04 19:33:09 +0100

    [frontend] code refactoring, object orientation, removed config -> configuration via js-calls in html

--- highcharts-frontend/dashboard.js
+++ highcharts-frontend/dashboard.js
@@ -1,16 +1,22 @@
-Highcharts.setOptions({
-    global: {
-        useUTC: false
+function formatData(value, suffix) {
+    var i = 0;
+    while (value >= 1000) {
+        i++;
+        value /= 1000;
     }
-});
+    return value.toPrecision(3) + " " + ["", "k", "M", "G", "T", "E"][i] + suffix;
+}
 
 
 
-function ChartSeriesTarget(series) {
+function ChartSeriesTarget(series, maxvalues) {
     this.series = series;
+    this.maxvalues = maxvalues;
 }
 ChartSeriesTarget.prototype.add = function(time, value) {
-    this.series.addPoint([time, value], false, false);
+    // shift chart if maximum number of values is already stored
+    var shift = (this.series.data.length >= this.maxvalues);
+    this.series.addPoint([time, value], false, shift);
 }
 ChartSeriesTarget.prototype.populate = function(list) {
     this.series.setData(list);
@@ -19,15 +25,19 @@ ChartSeriesTarget.prototype.populate = function(list) {
 
 
 function HtmlTarget(element, formatter) {
+    if (typeof(element) == "string") {
+        element = document.getElementById(element);
+    }
     this.element = element;
+    this.maxvalues = 1;
     this.formatter = formatter;
 }
 HtmlTarget.prototype.add = function(time, value) {
     if (this.formatter == undefined) {
-        this.element.innerHtml = value;
+        this.element.innerHTML = value;
     }
     else {
-        this.element.innerHtml = formatter(value);
+        this.element.innerHTML = this.formatter(value);
     }
 }
 HtmlTarget.prototype.populate = function(list) {
@@ -38,107 +48,118 @@ HtmlTarget.prototype.populate = function(list) {
 
 
 function Dashboard() {
-    this.initCharts();
-    this.fetchHistory();
+    this.charts = {};
+    this.targets = []; // list of key/value pairs
+    this.updateInterval = 10; // default value
 }
-Dashboard.prototype.initCharts = function() {
-    CHARTS = {};
-    targets = {};
-    MAXVALUES = {};
-
-    jQuery.getJSON('config.json', function(data) {
-        var container = document.getElementById('container');
-
-        for (var chartnum = 0; chartnum < data.charts.length; chartnum++) {
-            var chart = data.charts[chartnum];
-            var title = chart.name;
-
-            var div = document.createElement('div');
-            div.id = title;
-            div.setAttribute("class", "chart");
-            container.appendChild(div);
-
-            var settings = {
-                chart: {
-                    renderTo: title, // div with id from title field
-                    type: 'areaspline',
-                    marginRight: 10
-                },
-                title: {
-                    text: title
-                },
-                yAxis: {
-                    title: {
-                        text: chart.unit
-                    }
-                },
-                series: []
-            };
-            if (chart.type == "integer") {
-                settings.yAxis.allowDecimals = false;
-            }
-
-            var c = new Highcharts.Chart(settings);
-            CHARTS[title] = c;
-
-            for (var g = 0; g < chart.graphs.length; g++) {
-                var graph = chart.graphs[g];
-
-                var s = c.addSeries({
-                    name: graph.name,
-                    dataset: graph.dataset,
-                    data: []
-                });
-
-                targets[graph.dataset] = new ChartSeriesTarget(s);
-                MAXVALUES[graph.dataset] = chart.maxvalues;
-            }
-        }
-        element = document.getElementById("loading_msg");
-        element.parentNode.removeChild(element);
-    });
+Dashboard.prototype.addTarget = function(source, target) {
+    // data from source dataset gets added to the target
+    if (!this.targets[source]) {
+        this.targets[source] = [];
+    }
+    this.targets[source].push(target);
 }
 Dashboard.prototype.fetchHistory = function() {
-
-    // store local "this" for use in setInterval callback
+    // store dashboard context
     var t = this;
 
     jQuery.getJSON('/cgi-bin/history.json.py', function(data) {
         var interval = data.meta.interval;
+        t.updateInterval = interval;
         var lastrefresh = data.meta.refresh;
         for (var name in data.history) {
-            var values = data.history[name].values;
-            var length = values.length;
-            var firstelement = 0;
-            var maxvalues = MAXVALUES[name];
-            if (length > maxvalues) {
-                firstelement = length - maxvalues;
+            for (var tnum in t.targets[name]) {
+                var target = t.targets[name][tnum];
+                var values = data.history[name].values;
+                var length = values.length;
+                var firstelement = 0;
+                var maxvalues = target.maxvalues;
+                if (length > maxvalues) {
+                    firstelement = length - maxvalues;
+                }
+                var points = [];
+                for (var i = firstelement; i < length; i++) {
+                    var stepsbehind = i + 1 - length;
+                    var x = (lastrefresh + (interval * stepsbehind)) * 1000;
+                    points.push([x, values[i]]);
+                }
+                target.populate(points);
             }
-            var points = [];
-            for (var i = firstelement; i < length; i++) {
-                var stepsbehind = i + 1 - length;
-                var x = (lastrefresh + (interval * stepsbehind)) * 1000;
-                points.push([x, values[i]]);
-            }
-            targets[name].populate(points);
         }
-        for (var chartname in CHARTS) {
-            CHARTS[chartname].redraw();
+        for (var chartname in t.charts) {
+            t.charts[chartname].redraw();
         }
-        // set interval to update data every n seconds
-        setInterval(function(){t.fetchUpdate();}, interval*1000);
     });
 }
 Dashboard.prototype.fetchUpdate = function() {
+    // store dashboard context
+    var t = this;
+
     jQuery.getJSON('/cgi-bin/latest.json.py', function(data) {
         var lastrefresh = data.meta.refresh;
         for (var name in data.latest) {
-            var set = data.latest[name];
-            targets[name].add(lastrefresh*1000, set.value);
+            for (var tnum in t.targets[name]) {
+                var target = t.targets[name][tnum];
+                var set = data.latest[name];
+                target.add(lastrefresh*1000, set.value);
+            }
         }
-        for (var chartname in CHARTS) {
-            CHARTS[chartname].redraw();
+        for (var chartname in t.charts) {
+            t.charts[chartname].redraw();
         }
     });
 }
+Dashboard.prototype.scheduleUpdates = function() {
+    // set interval to update data every n seconds
+    var t = this;
+    setInterval(function(){t.fetchUpdate();}, t.updateInterval*1000);
+}
+Dashboard.prototype.addChart = function(chart, title) {
+    this.charts[title] = chart;
+}
+Dashboard.prototype.getChartByName = function(name) {
+    return this.charts[name];
+}
+Dashboard.prototype.getTargetsByDatasetName = function(name) {
+    return this.targets[name];
+}
+Dashboard.prototype.createChart = function(container, title, ylabel, datatype) {
+    if (typeof(container) == "string") {
+        container = document.getElementById(container);
+    }
+    var settings = {
+        chart: {
+            renderTo: container,
+            type: 'areaspline',
+            marginRight: 10
+        },
+        title: {
+            text: title
+        },
+        yAxis: {
+            title: {
+                text: ylabel
+            }
+        },
+        series: []
+    };
+    if (datatype == "integer") {
+        settings.yAxis.allowDecimals = false;
+    }
 
+    var chart = new Highcharts.Chart(settings);
+    this.addChart(chart, title);
+    return chart;
+}
+Dashboard.prototype.addSeriesToChart = function(chart, title, dataset, maxvalues) {
+    if (typeof(chart) == "string") {
+        chart = this.getChartByName(chart);
+    }
+
+    var series = chart.addSeries({
+        name: title,
+        data: []
+    });
+    this.addTarget(dataset, new ChartSeriesTarget(series, maxvalues));
+    return series;
+}
