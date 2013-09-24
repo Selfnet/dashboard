21840f11d200e8b61de86f2a67f76830833ed564
Sebastian Neuner <sebastiann@selfnet.de>
2013-09-24 14:38:44 +0200

    new configuration for previous commit (munin names instead of commands)

--- backend/conf.d/18-nat.py
+++ backend/conf.d/18-nat.py
@@ -1,14 +1,14 @@
 conf.add(Munin(
     name="cgn flows patty",
     host="patty",
-    cmd="fetch fw_conntrack\nquit",
+    identifier="fw_conntrack",
     key="total"
 ))
 
 conf.add(Munin(
     name="cgn flows marge",
     host="marge",
-    cmd="fetch fw_conntrack\nquit",
+    identifier="fw_conntrack",
     key="total"
 ))
 
