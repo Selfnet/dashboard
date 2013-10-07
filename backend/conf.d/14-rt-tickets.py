8f3f8c297e500ed8611cecf4d0cb8a168924fc93
Sebastian Neuner <sebastiann@selfnet.de>
2013-10-07 15:54:01 +0200

    fix: forgot to rename rt tickets

--- backend/conf.d/14-rt-tickets.py
+++ backend/conf.d/14-rt-tickets.py
@@ -1,11 +1,11 @@
 conf.add(HTTP(
-    "open selfnet rt tickets raw",
+    "open selfnet rt tickets",
     "https://rt.selfnet.de:444/tickets.pl",
     int
 ))
 
 conf.add(HTTP(
-    "open wh-netz rt tickets raw",
+    "open wh-netz rt tickets",
     "https://rt.selfnet.de:444/tickets-wh.pl",
     int
 ))
