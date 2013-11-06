54349c94bdd0be997484c1eb8f41186bbb52afb0
Sebastian Neuner <sebastiann@selfnet.de>
2013-11-06 01:00:44 +0100

    backend config: added usv4

--- backend/conf.d/12-usv.py
+++ backend/conf.d/12-usv.py
@@ -26,3 +26,11 @@ conf.add(SNMP(
     }
 ))   
 
+conf.add(SNMP(
+    version = '1',
+    host="10.0.15.2",
+    oids={
+        "usv4 current": ("iso.3.6.1.2.1.33.1.4.4.1.3.1", divide_by_ten),
+        "usv4 rack temperature": ("iso.3.6.1.4.1.705.1.8.1.0", divide_by_ten)
+    }
+))
