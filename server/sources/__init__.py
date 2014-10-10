c4f30aef3ffc39daba2e29cc2f061e70afbd9c3f
Sebastian Neuner <sebastiann@selfnet.de>
2014-10-11 00:38:55 +0200

    new source: run commands as subprocesses

--- server/sources/__init__.py
+++ server/sources/__init__.py
@@ -3,5 +3,6 @@ from .http import *
 from .snmp import *
 from .munin import *
 from .ping import *
+from .cmd import *
 from .printer import *
 from .conversion import *
---
+++ server/sources/cmd.py
@@ -0,0 +1,25 @@
+import logging
+import subprocess
+
+from .base import TimedSource
+
+
+
+class Cmd(TimedSource):
+
+    def cmdcall(self, args):
+        process = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True)
+        out, err = process.communicate()
+        return out
+
+    def poll(self):
+        cmd = self.get_config("command")
+        try:
+            out = self.cmdcall(cmd)
+            self.push(out.strip())
+        except Exception as e:
+            logging.error(" ".join([
+                type(e).__name__ + ":",
+                str(e),
+                "in Cmd for \"{name}\"".format(name=self.get_config("name"))
+            ]))
