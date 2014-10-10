c01ac98af7d6020814569a1fcc003866dd062004
Sebastian Neuner <sebastiann@selfnet.de>
2014-10-11 00:38:14 +0200

    ping6 (configurable via "protocol": "IPv6")

--- server/sources/ping.py
+++ server/sources/ping.py
@@ -7,9 +7,13 @@ from .base import TimedSource
 
 class Ping(TimedSource):
 
-    def pingcall(self, host, count=1, timeout=1):
+    def pingcall(self, host, count=1, timeout=1, protocol=4):
+        if protocol == 4:
+            cmd = "ping"
+        else:
+            cmd = "ping6"
         args = [
-            "ping",
+            cmd,
             "-c", "1",
             "-i", "0.2",
             "-c", str(count),
@@ -24,8 +28,13 @@ class Ping(TimedSource):
         host = self.get_config("host")
         count = self.get_config("count", 1)
         timeout = self.get_config("timeout", 1)
+        protocol = self.get_config("protocol", 4)
+        if protocol == 6 or str(protocol)[-1] == "6":
+            protocol = 6
+        else:
+            protocol = 4
         try:
-            out = self.pingcall(host, count, timeout)
+            out = self.pingcall(host, count, timeout, protocol)
             lastline = out.strip().split(b"\n")[-1]
             if lastline.startswith(b"rtt min/avg/max/mdev = "):
                 rtt = float(lastline.split(b"/")[4])
