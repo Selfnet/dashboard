90f193a34fba2b093e8c08fb1983c71d8184af4c
Sebastian Neuner <sebastian@sneuner.org>
2015-12-22 23:42:33 +0100

    added argument parser to set loglevel

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,24 @@
 #! /usr/bin/env python3
 import logging
-
+import argparse
 import config
 import storage
 from threadmanagement import WorkerThreads
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--loglevel", metavar="LEVEL", type=str,
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="Loglevel (DEBUG, INFO, WARNING, ERROR, CRITICAL)")
+    args = parser.parse_args()
+
+    if args.loglevel:
+        loglevel = getattr(logging, args.loglevel.upper(), None)
+        if loglevel:
+            logging.basicConfig(level=loglevel)
+            logging.info("set loglevel to " + str(args.loglevel))
+
     # get the configuration
     conf = config.parse_config()
     stor = storage.Storage()
