679314581d9fef2020b39b3cc406d2629045cac4
Sebastian Neuner <sebastian@sneuner.org>
2016-01-17 01:41:41 +0100

    improved parser modules and added YAML

--- modules/parsers.py
+++ modules/parsers.py
@@ -1,33 +1,54 @@
 import logging
 from operator import itemgetter
 import json
+import yaml
 
 from .base.sources import PubSubSource
 
 
-class JSONParser(PubSubSource):
+class Parser(PubSubSource):
+
+    def parse(self, data):
+        pass
+
+    def getitem(self, data, path):
+        if isinstance(path, list):
+            for item in path:
+                getter = itemgetter(item)
+                data = getter(data)
+            return data
+        else:
+            return data[path]
+
     def update(self, channel, timestamp, value):
         # get parameters
         path = self.get_config("path", [])
 
         # get the data
         try:
-            value = json.loads(value)
+            value = self.parse(value)
         except Exception as e:
-            logging.warning("Error in JSONParser: " + str(e))
+            logging.warning(str(e))
             return
 
-        # get the right element
-        if path and isinstance(path, str):
-            path = [path]
-        if path and isinstance(path, list):
-            for item in path:
-                getter = itemgetter(item)
-                try:
-                    value = getter(value)
-                except:
-                    logging.info("Could not get item \"{key}\"".format(key=str(item)))
-                    return
+        # if a path is set, try to get the item
+        if path:
+            try:
+                value = self.getitem(value, path)
+            except Exception as e:
+                logging.info(str(e))
 
         # write the data
         self.push(value)
+
+
+class JSONParser(Parser):
+
+    def parse(self, data):
+        return json.loads(data)
+
+
+class YAMLParser(Parser):
+
+    def parse(self, data):
+        return yaml.load(data)
